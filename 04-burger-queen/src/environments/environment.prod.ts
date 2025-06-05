7e153e12556607efcdf72d7777f4a36c0e189e60
giojibo <bonillagiovani6@gmail.com>
2025-06-05 17:34:45 -0600

    Burger Queen App

--- 04-burger-queen/src/environments/environment.prod.ts
+++ 04-burger-queen/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  urlApi: 'http://192.168.1.72:3000/api/v1/',
-  publishKey: 'pk_test_51RWPU2FkPmsTPysaFBKdmBpGbk3iSUZORComn55iUAoAciMYscgHz0wdEAom7uiJ3lrhehKe9vSW0pp7M0Agd2Rm00NnVZNgtK',
-  secretKey: 'sk_test_51RWPU2FkPmsTPysaFMU1wc7RjHTROn1f4qUWwrGiM7AMOfwcuMK8EsLDrNpYgGE2xxu6e7DY2DYe0qyrPCwGIZcy00RoujPNjn',
+  urlApi: 'http://ip:3000/api/v1/',
+  publishKey: '',
+  secretKey: '',
 };
 
--- 04-burger-queen/src/environments/environment.ts
+++ 04-burger-queen/src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   urlApi: 'http://localhost:3000/api/v1/',
-  publishKey: 'pk_test_51RWPU2FkPmsTPysaFBKdmBpGbk3iSUZORComn55iUAoAciMYscgHz0wdEAom7uiJ3lrhehKe9vSW0pp7M0Agd2Rm00NnVZNgtK',
-  secretKey: 'sk_test_51RWPU2FkPmsTPysaFMU1wc7RjHTROn1f4qUWwrGiM7AMOfwcuMK8EsLDrNpYgGE2xxu6e7DY2DYe0qyrPCwGIZcy00RoujPNjn',
+  publishKey: '',
+  secretKey: '',
 };
 
 
